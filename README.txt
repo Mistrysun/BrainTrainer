10403565e738e6a46f11859ced59ba19b4f1e118
Sundip <mistrysun@gmail.com>
2019-02-14 16:33:04 +0000

    1.0

---
+++ README.txt
@@ -0,0 +1,25 @@
++---------+------------+-----------------------------------------------------------------------------------+
+| Version | Date       | Content                                                                           |
++---------+------------+-----------------------------------------------------------------------------------+
+| 0.1     | 13/02/2019 | - Creating a Go Button                                                            |
+|         |            | - Button when clicked will make Button Invisible                                  |
++---------+------------+-----------------------------------------------------------------------------------+
+| 0.2     | 13/02/2019 | - GUI Created                                                                     |
+|         |            | - No functions                                                                    |
++---------+------------+-----------------------------------------------------------------------------------+
+| 0.5     | 13/02/2019 | - Creating Random Number Generator                                                |
+|         |            | - Output to Log                                                                   |
++---------+------------+-----------------------------------------------------------------------------------+
+| 0.7     | 13/02/2019 | - for statement created to generate 4 numbers.                                    |
+|         |            | - 3 of which are random                                                           |
+|         |            | - 1 of which stores the correct number in var                                     |
+|         |            | - These are added to an ArrayList                                                 |
+|         |            | - Each button is located and number is set from Array List                        |
++---------+------------+-----------------------------------------------------------------------------------+
+| 0.9     | 13/02/2019 | - Sometimes you'll find the number randomly generator will appear twice on Button |
+|         |            | - To avoid this, a while statement is inserted within the 'for' statement.        |
+|         |            | - The while statement will check if random number Generated == a+b.               |
+|         |            |    - If so, then regenerate number.                                               |
++---------+------------+-----------------------------------------------------------------------------------+
+| 1.0     | 13/02/2019 | - App now works functionally                                                      |
++---------+------------+-----------------------------------------------------------------------------------+
--- app/src/main/java/com/example/android/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/android/braintrainer/MainActivity.java
@@ -1,16 +1,87 @@
 package com.example.android.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    // to randomise where the real answer button will be.
+    // here you randomise 4 number
+    // button has not yet been set
+    int locationOfCorrectAnswer;
+    int score = 0;
+    int numberOfQuestions = 0;
+    TextView scoreTextView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    TextView sumTextView;
+    TextView timerTextView;
+
     Button goButton;
+    TextView resultTextView;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
+
+    public void chooseAnswer(View view) {
+
+        //check locationOfCorrectAnswer = button tag
+        if (Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())) {
+            resultTextView.setText("Correct!");
+            score++;
+        } else {
+            resultTextView.setText("Wrong :(");
+        }
+        numberOfQuestions++;
+        scoreTextView.setText(Integer.toString(score)+ "/" + Integer.toString(numberOfQuestions));
+        newQuestion();
+    }
 
     public void Start(View view) {
         goButton.setVisibility(View.INVISIBLE);
+    }
+
+    public void newQuestion() {
+        Random rand = new Random();
+
+        //bound = 0 -> 20 [gives range]
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+
+        //must clear the ArrayList otherwise old number will retain
+        answers.clear();
+
+        //increments 0 to 3
+        for (int i = 0; i<4; i++) {
+            if ( i == locationOfCorrectAnswer) {
+                answers.add(a+b);
+            } else {
+                int wrongAnswer = rand.nextInt(41);
+
+                while (wrongAnswer == a+b) {
+                    wrongAnswer = rand.nextInt(41);
+                }
+
+                answers.add(wrongAnswer);
+            }
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 
     @Override
@@ -18,6 +89,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sumTextView = findViewById(R.id.sumTextView);
+        resultTextView = findViewById(R.id.resultTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
         goButton = findViewById(R.id.goButton);
+        timerTextView = findViewById(R.id.timerTextView);
+
+        newQuestion();
+
+        new CountDownTimer(30100, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                timerTextView.setText(String.valueOf(l /1000) + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                resultTextView.setText("Done");
+            }
+        }.start();
     }
 }
